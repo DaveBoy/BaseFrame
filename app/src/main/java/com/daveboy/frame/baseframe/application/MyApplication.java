4f379f27c8086ce65e219644408db630fafa1803
mxm <1130198893@qq.com>
2018-09-19 13:34:29 +0800

    集成MvpArms,去除组件化

---
+++ app/src/main/java/com/daveboy/frame/baseframe/application/MyApplication.java
@@ -0,0 +1,15 @@
+package com.daveboy.frame.baseframe.application;
+
+import android.app.Application;
+
+
+
+public class MyApplication extends Application {
+    public static Application mApplication;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mApplication=this;
+
+    }
+}
